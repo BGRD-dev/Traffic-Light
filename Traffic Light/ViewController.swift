659ece6753a095d2bcae5d296ba9e6bd272ba832
Boris <brsbgrd@gmail.com>
2020-05-20 14:53:31 -0400

    everything is working

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var actionButton: UIButton!
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.3
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        
+        yellowLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.width / 2
+        
+        greenLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+        
+        actionButton.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func actionButtonPressed() {
+        if redLightView.alpha != 1 && yellowLightView.alpha != 1 {
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+            actionButton.setTitle("NEXT", for: .normal)
+        }else if redLightView.alpha == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        }else{
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        }
+    }
+    
 }
 
